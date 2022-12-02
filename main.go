77221bf62e95864c498a070b4089902e2b1a329a
Clg <cg@jmg-conseil.eu>
2022-12-02 15:54:34 +0100

    added csv and read func

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"github.com/go-gota/gota/dataframe"
-	"github.com/go-gota/gota/series"
 )
 
 func main() {
+	
+	file, err := os.Open("data/frequentation-gares.csv")
+	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	df := dataframe.ReadCSV(file)
 
+	fmt.Println(df)
 }
