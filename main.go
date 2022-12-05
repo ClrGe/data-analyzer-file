140d7a80a6ffc221b94ae8680c0dac8a9188ea35
Clg <cg@jmg-conseil.eu>
2022-12-05 10:37:45 +0100

    setup

--- main.go
+++ main.go
@@ -5,10 +5,53 @@ import (
 	"log"
 	"os"
 	"github.com/go-gota/gota/dataframe"
+	"encoding/json"
+	"net/http"
+	"io/ioutil"
+	"time"
 )
+
+type Stations struct {
+	UIC []string `json:[fields.code_uic]`
+}
 
 func main() {
 	
+	url := "https://lab.jmg-conseil.eu/db/all"
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	spaceClient := http.Client{
+		Timeout: time.Second * 232, // Timeout after 2 seconds
+	}
+
+	req.Header.Set("User-Agent", "service")
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	station := Stations{}
+
+	jsonErr := json.Unmarshal(body, &station)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	fmt.Println(station.UIC)
+
 	file, err := os.Open("data/frequentation-gares.csv")
 	defer file.Close()
 	if err != nil {
