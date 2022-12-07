6faa44cd07f09c0ae75eac4f1fc5df730d2fec5b
Lokda-spec <gouarnec@gmail.com>
2022-12-07 11:53:02 +0100

    cleaned up workspace

--- main.go
+++ main.go
@@ -11,59 +11,40 @@ import (
 )
 
 // defining the station struct
-type Stations []struct {
-	Datasetid string `json:"datasetid"`
-	Fields    struct {
-		GareAliasLibelleNoncontraint string `json:"gare_alias_libelle_noncontraint"`
-		CommuneLibellemin            string `json:"commune_libellemin"`
-		UicCode                      string `json:"uic_code"`
-		Gare                         string `json:"gare"`
-		AdresseCp                    string `json:"adresse_cp"`
-		DepartementLibellemin        string `json:"departement_libellemin"`
-		GareRegionsncfLibelle        string `json:"gare_regionsncf_libelle"`
-	} `json:"fields"`
-}
-
-// defining the station struct
 type StationData [][]string
 
-// retrieve stations ref-data from API
-func getRef(w http.ResponseWriter, r *http.Request) {
-
-	//zipcode := "76000"
-	url := "http://127.0.0.1:8200/cell/test"
-
-	var station Stations
-
-	// Build the request
-	req, err := http.NewRequest("GET", url, nil)
+// convert .csv file
+func convertData() {
+	file, err := os.Open("data/gares.csv")
+	defer file.Close()
 	if err != nil {
-		log.Fatal("NewRequest: ", err)
-		return
+		log.Fatal(err)
 	}
+	df := dataframe.ReadCSV(file)
+	columns := df.Select([]string{"nom_gare", "code_uic_complet", "total_voyageurs_non_voyageurs_2015", "total_voyageurs_non_voyageurs_2016", "total_voyageurs_non_voyageurs_2017", "total_voyageurs_non_voyageurs_2018", "total_voyageurs_non_voyageurs_2019", "total_voyageurs_non_voyageurs_2020", "total_voyageurs_non_voyageurs_2021"})
 
-	client := &http.Client{}
-
-	resp, err := client.Do(req)
+	fmt.Println(columns)
+	nile, err := os.Create("data/output.json")
 	if err != nil {
-		log.Fatal("Do: ", err)
-		return
+		log.Fatal(err)
 	}
-	defer resp.Body.Close()
+	columns.WriteJSON(nile)
+}
 
-	// Use json.Decode for reading streams of JSON data
-	if err := json.NewDecoder(resp.Body).Decode(&station); err != nil {
-		log.Println(err)
-	}
-
-	fmt.Fprintf(w, "data: %s\n", station)
+// serve converted data
+func serveJson(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "data/output.json")
 }
 
 // retrieve stations ref-data from API
 func getApi(w http.ResponseWriter, r *http.Request) {
 
-	//zipcode := "76000"
-	url := "https://lab.jmg-conseil.eu/db/all"
+	// retrieve request parameters
+	uiccode := r.URL.Query()["uic"]
+	zipcode := r.URL.Query()["zipcode"]
+
+	// base url from which the data will be fetched
+	url := "https://lab.jmg-conseil.eu/db/search?zipcode=" + zipcode[0]
 
 	var stationf StationData
 
@@ -88,53 +69,27 @@ func getApi(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 
-	fmt.Fprintf(w, "data: %s\n", stationf)
-}
-
-// convert .csv file
-func convertData() {
-	file, err := os.Open("data/gares.csv")
-	defer file.Close()
-	if err != nil {
-		log.Fatal(err)
+	if uiccode != nil {
+		fmt.Fprintf(w, "Parametre de recherche : Code UIC %s\n", uiccode)
 	}
-	df := dataframe.ReadCSV(file)
-	columns := df.Select([]string{"nom_gare", "code_uic_complet", "total_voyageurs_non_voyageurs_2015", "total_voyageurs_non_voyageurs_2016", "total_voyageurs_non_voyageurs_2017", "total_voyageurs_non_voyageurs_2018", "total_voyageurs_non_voyageurs_2019", "total_voyageurs_non_voyageurs_2020", "total_voyageurs_non_voyageurs_2021"})
 
-	fmt.Println(columns)
-	nile, err := os.Create("data/output.json")
-	if err != nil {
-		log.Fatal(err)
+	if zipcode != nil {
+		fmt.Fprintf(w, "Parametre de recherche : Code postal %s\n", zipcode)
 	}
-	columns.WriteJSON(nile)
-}
 
-// calculate sum of travellers for the period 2015-2021
-func getSum() {
-	file, err := os.Open("data/gares.csv")
-	defer file.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	df := dataframe.ReadCSV(file)
-	columns := df.Select([]string{"nom_gare", "code_uic_complet", "total_voyageurs_non_voyageurs_2015", "total_voyageurs_non_voyageurs_2016", "total_voyageurs_non_voyageurs_2017", "total_voyageurs_non_voyageurs_2018", "total_voyageurs_non_voyageurs_2019", "total_voyageurs_non_voyageurs_2020", "total_voyageurs_non_voyageurs_2021"})
-
-	fmt.Println(columns)
-	nile, err := os.Create("data/output.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	columns.WriteJSON(nile)
-}
-
-// serve converted data
-func serveJson(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "data/output.json")
-}
-
-// serve converted data
-func test(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "data/referentiel-gares-voyageurs.json")
+	/* check for a matching zipcode
+	var result bool = false
+	for i := 0; i < len(stationf); i++ {
+		if stationf[i][1] == zipcode {
+			result = true
+			fmt.Fprintf(w, "Code postal: %s\n", stationf[i])
+			break
+		}
+	}*/
+	fmt.Fprintf(w, "Commune: %s\n", stationf[0][0])
+	fmt.Fprintf(w, "Département: %s\n", stationf[0][2])
+	fmt.Fprintf(w, "Region: %s\n", stationf[0][1])
+	fmt.Fprintf(w, "Code UIC: %s\n", stationf[0][4])
 }
 
 // main function
@@ -143,9 +98,7 @@ func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/cell", serveJson).Methods("GET")
-	router.HandleFunc("/cell/test", test).Methods("GET")
 	router.HandleFunc("/cell/api", getApi).Methods("GET")
-	router.HandleFunc("/cell/ref", getRef).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8200", router))
 
