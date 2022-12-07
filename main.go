3379c69a5c59065c5a6131564c1fe4bd52fa4c43
Lokda-spec <gouarnec@gmail.com>
2022-12-07 15:42:21 +0100

    retrieve data from csv in df

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/go-gota/gota/dataframe"
+	"github.com/go-gota/gota/series"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
@@ -14,21 +15,35 @@ import (
 type StationData [][]string
 
 // convert .csv file
-func convertData() {
+func convertData(w http.ResponseWriter, r *http.Request) {
 	file, err := os.Open("data/gares.csv")
 	defer file.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
 	df := dataframe.ReadCSV(file)
-	columns := df.Select([]string{"nom_gare", "code_uic_complet", "total_voyageurs_non_voyageurs_2015", "total_voyageurs_non_voyageurs_2016", "total_voyageurs_non_voyageurs_2017", "total_voyageurs_non_voyageurs_2018", "total_voyageurs_non_voyageurs_2019", "total_voyageurs_non_voyageurs_2020", "total_voyageurs_non_voyageurs_2021"})
+	df.SetNames("datasetid", "recordid", "total_2018", "total_2021", "total_2015", "seg_drg", "total_2020", "total_2016", "total_2018", "total2017", "total_2017", "total_2019", "code_uic", "cp", "total_2020", "total2019", "total2021", "total2016", "total2015", "nom_gare", "date")
 
-	fmt.Println(columns)
-	nile, err := os.Create("data/output.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	columns.WriteJSON(nile)
+	/*	columns := df.Select([]string{"nom_gare", "code_uic_complet", "total_voyageurs_non_voyageurs_2015", "total_voyageurs_non_voyageurs_2016", "total_voyageurs_non_voyageurs_2017", "total_voyageurs_non_voyageurs_2018", "total_voyageurs_non_voyageurs_2019", "total_voyageurs_non_voyageurs_2020", "total_voyageurs_non_voyageurs_2021"})
+
+		fmt.Println(df)
+		nile, err := os.Create("data/output.json")
+		if err != nil {
+			log.Fatal(err)
+		}
+		columns.WriteJSON(nile)*/
+	uiccode := r.URL.Query()["uic"]
+
+	fil := df.Filter(
+		dataframe.F{
+			Colname:    "code_uic",
+			Comparator: series.Eq,
+			Comparando: uiccode,
+		},
+	)
+
+	fmt.Fprint(w, fil.Select([]int{2, 3, 4, 6, 7, 8, 10, 11, 14}))
+
 }
 
 // serve converted data
@@ -43,8 +58,20 @@ func getApi(w http.ResponseWriter, r *http.Request) {
 	uiccode := r.URL.Query()["uic"]
 	zipcode := r.URL.Query()["zipcode"]
 
-	// base url from which the data will be fetched
+	//  url from which the referential data will be fetched
 	url := "https://lab.jmg-conseil.eu/db/search?zipcode=" + zipcode[0]
+
+	// url from which the yearly freq data will be fetched
+	//urlFreq := "https://lab.jmg-conseil.eu/cell"
+
+	file, err := os.Open("data/frequentation-gares.csv")
+	defer file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	df := dataframe.ReadCSV(file)
+	infoGare := df.Select([]string{"Code UIC"})
+	fmt.Fprintf(w, "Test avec CSV %s\n", infoGare)
 
 	var stationf StationData
 
@@ -94,9 +121,9 @@ func getApi(w http.ResponseWriter, r *http.Request) {
 
 // main function
 func main() {
-	convertData()
-
 	router := mux.NewRouter()
+	router.HandleFunc("/cell/ok", convertData).Methods("GET")
+
 	router.HandleFunc("/cell", serveJson).Methods("GET")
 	router.HandleFunc("/cell/api", getApi).Methods("GET")
 
